34e01e59fc451b0371de66520edd330538564ed0
Aldy Prastyo <dipras75@gmail.com>
2025-12-19 13:00:09 +0700

    feat: Integrate all components in main application

--- src/main/java/com/otp/App.java
+++ src/main/java/com/otp/App.java
@@ -1,9 +1,12 @@
 package com.otp;
-
+import com.otp.ui.TUIManager;
 public class App {
     public static void main(String[] args) {
-        
-        System.out.println("Hello World!");
+        System.out.println("ON-TIME PATH - Sampai Kampus Tepat Waktu!");
+        System.out.println("===========================================");
+        System.out.println("Memuat game...\n");
+        TUIManager tui = new TUIManager();
+        tui.run();
+        System.out.println("\nTerima kasih sudah bermain!");
     }
 }
-
